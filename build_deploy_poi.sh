0dc9774072e6c045126a490cb58a5f9531210587
Pieter de Bruin <pdebruin@hotmail.com>
2020-01-06 16:35:12 +0100

    helm upgrade

--- build_deploy_poi.sh
+++ build_deploy_poi.sh
@@ -124,10 +124,11 @@ echo -e "\nSuccessfully pushed image: "$TAG
 popd
 
 #installPath=$relativeSaveLocation"/openhack-devops-team/apis/poi/charts/mydrive-poi"
-installPath=./apis/poi/charts/mydrive-poi
+installPath="apis/poi/charts/mydrive-poi"
 echo -e "\nhelm install from: " $installPath "\n\n"
 
 BASE_URI='http://'$dnsUrl
 echo "Base URI: $BASE_URI"
 
-helm install $installPath --name api-poi --set repository.image=$TAG,env.webServerBaseUri=$BASE_URI,ingress.rules.endpoint.host=$dnsUrl
+#helm install $installPath --name api-poi --set repository.image=$TAG,env.webServerBaseUri=$BASE_URI,ingress.rules.endpoint.host=$dnsUrl
+helm upgrade api-poi $installPath
